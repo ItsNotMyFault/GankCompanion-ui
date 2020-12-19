c0824030d6a4d6fecef1316897b4a3a439b7ff0f
AlexisGuay <ItsNotMyFault_dev@Hotmail.com>
2020-12-19 18:19:09 -0500

    ui is looking great, rest table and some header changes.

--- src/app/party/graph-kills/graph-kills.component.ts
+++ src/app/party/graph-kills/graph-kills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-graph-kills',
@@ -10,6 +11,28 @@ export class GraphKillsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var lineChart = new Chart('myChart', {
+      type: 'line',
+      data: {
+        labels: ['', '', '', '', '', '', '', '', ''],//same count as the data.
+        datasets: [
+          { data: [5, 2, 8, 2, 32, 12, 7, 1, 9], borderColor: '#3cb371' }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{ display: true }],
+          yAxes: [{ display: true }]
+        }
+      }
+    });
+
   }
+
+
+
 
 }
